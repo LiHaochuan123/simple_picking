7e068bf66e97271394ea6fa14f9fa3fb1ea8243a
LiHaochuan <2502494553@qq.com>
2024-11-27 18:56:00 +0800

    add gitignore and add load_shape function

--- load.py
+++ load.py
@@ -1,14 +1,26 @@
-"""This module loads objects."""
+"""This module loading objects."""
 import time
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
-client = RemoteAPIClient()
-sim = client.require('sim')
-time.sleep(3)
-sim.startSimulation()
-shapeHandle = sim.createPrimitiveShape(
-    sim.primitiveshape_cuboid, [0.05, 0.05, 0.05])
-sim.setObjectPosition(shapeHandle, [0.5, 0.5, 0.5])
-sim.setBoolProperty(shapeHandle, 'respondable', True)
-sim.setBoolProperty(shapeHandle, 'dynamic', True)
-time.sleep(5)
-sim.stopSimulation()
+
+
+def load_shape(simulation: RemoteAPIClient.require) -> None:
+    """Function loading a cuobid in specific position.
+
+    args:
+        simulation: 
+    """
+    shape_handle = simulation.createPrimitiveShape(
+        simulation.primitiveshape_cuboid, [0.05, 0.05, 0.05])
+    simulation.setObjectPosition(shape_handle, [0.5, 0.5, 0.5])
+    simulation.setBoolProperty(shape_handle, 'respondable', True)
+    simulation.setBoolProperty(shape_handle, 'dynamic', True)
+
+
+if __name__ == "__main__":
+    client = RemoteAPIClient()
+    sim = client.require('sim')
+    time.sleep(3)
+    sim.startSimulation()
+    load_shape(simulation=sim)
+    time.sleep(5)
+    sim.stopSimulation()
